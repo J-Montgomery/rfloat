67e94139b3c04d75f42e76c658a9c4373bd5e842
J M <montytyper@msn.com>
2024-08-10 09:16:15 -0700

    Initial commit

---
+++ src/pregel_tests.cpp
@@ -0,0 +1,10 @@
+#include "gtest/gtest.h"
+
+#include "../headers/pregel.hh"
+
+class PregelTests : public ::testing::Test {};
+
+TEST_F(PregelTests, TestBasicFunctionality) {
+
+    EXPECT_EQ(1, 1);
+}
