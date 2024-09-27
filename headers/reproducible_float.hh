1e8d1f6a3e0711a524dc4d591b47a670c7387f86
J M <montytyper@msn.com>
2024-09-26 17:53:06 -0700

    Rework things a bit and clean up the code
    1) Throw roundingmodes and setroundingmode() into a namespace.
    2) Support fp16
    3) Move the operators back into the class
    4) Make external conversion operators that will safely encapsulate
       nonreproducible types.
    5) Fix the issue with ambiguous overloads for double and float
    6) Fix some nasty issues in tests that weren't noted by CC, but
    were discovered when built under clang and GCC directly.

--- headers/reproducible_float.hh
+++ headers/reproducible_float.hh
@@ -4,6 +4,11 @@
 #include <limits>
 #include <type_traits>
 
+#if __cplusplus >= 202302L
+/* Include the stdfloat header if it exists */
+#include <stdfloat>
+#endif
+
 // Most compilers don't support this pragma and ignore it, but
 // we're still technically required to set it
 #pragma STDC FENV_ACCESS ON
@@ -22,23 +27,51 @@
 #define OPT_BARRIER(param)
 #endif
 
+#if __cplusplus >= 202002L
+#define FEATURE_CXX20(expr) expr
+#else
+#define FEATURE_CXX20(expr)
+#endif
+
+// Our safety checks are taken care of at the usage site
 #define SAFE_BINOP(result, a, b, op)                                           \
-    typeof(a) result = (a)op(b);                                               \
+    T result = (a)op(b);                                                       \
     OPT_BARRIER(result);
 
-enum class RoundingMode {
-    RoundToEven,
-    RoundToPositive,
-    RoundToNegative,
-    RoundToZero
-};
+namespace rfp {
 
-template <typename T, RoundingMode R = RoundingMode::RoundToEven,
-          bool init_rounding = false>
+enum class RoundingMode { ToEven, ToPositive, ToNegative, ToZero };
+
+template <RoundingMode R> void SetRoundingMode() {
+    if (R == RoundingMode::ToEven) {
+        fesetround(FE_TONEAREST);
+    } else if constexpr (R == RoundingMode::ToPositive) {
+        fesetround(FE_UPWARD);
+    } else if constexpr (R == RoundingMode::ToNegative) {
+        fesetround(FE_DOWNWARD);
+    } else if constexpr (R == RoundingMode::ToZero) {
+        fesetround(FE_TOWARDZERO);
+    } else {
+        static_assert(R == RoundingMode::ToEven,
+                      "Rounding mode is not supported");
+    }
+}
+} // namespace rfp
+
+template <typename T, rfp::RoundingMode R = rfp::RoundingMode::ToEven>
 class NumericWrapper {
+#if __cplusplus >= 202302L
+    static_assert(std::is_same<T, float>::value ||
+                      std::is_same<T, double>::value ||
+                      std::is_same<T, std::float16_t>::value ||
+                      std::is_same<T, std::float32_t>::value ||
+                      std::is_same<T, std::float64_t>::value,
+                  "Unsupported floating point type");
+#else /* Pre-C++23 */
     static_assert(std::is_same<T, float>::value ||
                       std::is_same<T, double>::value,
                   "Unsupported floating point type");
+#endif
     static_assert(std::is_floating_point<T>::value,
                   "Non-floating point types are not supported");
     static_assert(std::numeric_limits<T>::is_iec559,
@@ -47,195 +80,203 @@ class NumericWrapper {
   public:
     T value;
 
-    explicit NumericWrapper(T val) : value(val) {
-        if constexpr (init_rounding) {
-            setround();
-        }
-    }
+    constexpr NumericWrapper(const T &val) : value(val) {}
 
-    NumericWrapper<T, R> &operator=(T val) {
+    template <rfp::RoundingMode R2>
+    constexpr NumericWrapper<T, R2> &operator=(T val) {
         value = val;
         return *this;
     }
 
-    template <RoundingMode R2>
-    NumericWrapper<T, R> &operator=(const NumericWrapper<T, R2> &other) {
+    template <rfp::RoundingMode R2>
+    constexpr NumericWrapper<T, R> &
+    operator=(const NumericWrapper<T, R2> &other) {
         value = other.value;
         return *this;
     }
 
-    // Delete implicit conversion operators
-    operator float() const = delete;
-    operator double() const = delete;
+    // Normally we'd want to delete the implicit conversion operators
+    // However, GCC incorrectly assumes that doing so means that
+    // the types are convertible and generates confusing errors about
+    // ambiguous overloads with floats and doubles.
+    // Therefore, we avoid explicitly deleting them.
+    // operator float() const = delete;
+    // operator double() const = delete;
 
-    inline double fp64() const { return static_cast<double>(value); }
+    constexpr inline double fp64() const { return static_cast<double>(value); }
 
     template <typename T2 = T, typename = typename std::enable_if_t<
                                    std::is_same<T2, float>::value>>
-    inline T2 fp32() const {
-        return static_cast<float>(value);
+    constexpr inline T2 fp32() const {
+        return value;
     }
 
-    void setround() {
-        if (R == RoundingMode::RoundToEven) {
-            fesetround(FE_TONEAREST);
-        } else if constexpr (R == RoundingMode::RoundToPositive) {
-            fesetround(FE_UPWARD);
-        } else if constexpr (R == RoundingMode::RoundToNegative) {
-            fesetround(FE_DOWNWARD);
-        } else if constexpr (R == RoundingMode::RoundToZero) {
-            fesetround(FE_TOWARDZERO);
-        } else {
-            static_assert(R == RoundingMode::RoundToEven,
-                          "Rounding mode is not supported");
-        }
+#if __cplusplus >= 202302L
+    template <typename T2 = T, typename = typename std::enable_if_t<
+                                   std::is_same<T2, std::float16_t>::value>>
+    constexpr inline T2 fp16() const {
+        return value;
+    }
+#endif
+
+#if __cplusplus >= 202002L
+    constexpr inline auto operator<=>(const NumericWrapper<T, R> &rhs) const {
+        return value <=> rhs.value;
+    };
+#endif
+
+    constexpr inline auto operator<(const NumericWrapper<T, R> &rhs) const {
+        return value < rhs.value;
+    };
+
+    constexpr inline auto operator>(const NumericWrapper<T, R> &rhs) const {
+        return value > rhs.value;
+    };
+
+    constexpr inline auto operator<=(const NumericWrapper<T, R> &rhs) const {
+        return value <= rhs.value;
+    };
+
+    constexpr inline auto operator>=(const NumericWrapper<T, R> &rhs) const {
+        return value >= rhs.value;
+    };
+
+    constexpr inline auto operator==(const NumericWrapper<T, R> &rhs) const {
+        return value == rhs.value;
+    };
+
+    constexpr inline auto operator!=(const NumericWrapper<T, R> &rhs) const {
+        return value != rhs.value;
+    };
+
+    FEATURE_CXX20(constexpr)
+    inline NumericWrapper<T, R>
+    operator+(const NumericWrapper<T, R> &rhs) const {
+        SAFE_BINOP(result, value, rhs.value, +);
+        return NumericWrapper(result);
+    }
+    FEATURE_CXX20(constexpr)
+    inline NumericWrapper<T, R>
+    operator-(const NumericWrapper<T, R> &rhs) const {
+        SAFE_BINOP(result, value, rhs.value, -);
+        return NumericWrapper(result);
+    }
+
+    FEATURE_CXX20(constexpr)
+    inline NumericWrapper<T, R>
+    operator*(const NumericWrapper<T, R> &rhs) const {
+        SAFE_BINOP(result, value, rhs.value, *);
+        return NumericWrapper(result);
+    }
+
+    FEATURE_CXX20(constexpr)
+    inline NumericWrapper<T, R>
+    operator/(const NumericWrapper<T, R> &rhs) const {
+        SAFE_BINOP(result, value, rhs.value, /);
+        return NumericWrapper(result);
     }
 };
 
-#if __cplusplus >= 202002
-template <RoundingMode R = RoundingMode::RoundToEven,
-          bool init_rounding = false>
-using rfloat = NumericWrapper<float, R, init_rounding>;
-template <RoundingMode R = RoundingMode::RoundToEven,
-          bool init_rounding = false>
-using rdouble = NumericWrapper<double, R, init_rounding>;
+#if __cplusplus >= 202002L
+// Use C++20 alias template type deduction if it's available
+template <rfp::RoundingMode R = rfp::RoundingMode::ToEven>
+using rfloat = NumericWrapper<float, R>;
+template <rfp::RoundingMode R = rfp::RoundingMode::ToEven>
+using rdouble = NumericWrapper<double, R>;
 #else
-using rfloat = NumericWrapper<float, RoundingMode::RoundToEven, false>;
-using rdouble = NumericWrapper<double, RoundingMode::RoundToEven, false>;
+// otherwise pick reasonable defaults
+using rfloat = NumericWrapper<float, rfp::RoundingMode::ToEven>;
+using rdouble = NumericWrapper<double, rfp::RoundingMode::ToEven>;
 #endif
 
-template <typename T1, typename T2>
-inline bool operator==(const NumericWrapper<T1> &lhs, const T2 &rhs) {
-    if constexpr (std::is_same<T2, float>::value) {
-        if constexpr (std::is_same<T1, T2>::value) {
-            // If we're both floats,
-            // then we can compare them without downcasting
-            return lhs.value == rhs;
-        } else {
-            // Otherwise, we have to upcast both to doubles to
-            // compare them
-            return lhs.value == static_cast<decltype(lhs.fp64())>(rhs);
-        }
-    } else if constexpr (std::is_same<T2, double>::value) {
-        return lhs.fp64() == rhs;
-    }
-}
+// Support expressions with non-reproducible types
+// on the left hand side by converting them to reproducible types
+// with the same type and rounding mode as the right hand side
+// first. This should be essentially free, and the type overloads
+// will ensure the operation is done safely.
 
-/* Relational operators */
+#if __cplusplus >= 202002L
+template <typename T, rfp::RoundingMode R>
+constexpr inline auto operator<=>(const T &lhs,
+                                  const NumericWrapper<T, R> &rhs) {
+    return NumericWrapper(lhs) <=> rhs;
+};
+#endif
 
-// Declare operator== as commutative to resolve the argument order ambiguity
-// for the compiler
-template <typename T1, typename T2>
-inline bool operator==(const T2 &lhs, const NumericWrapper<T1> &rhs) {
-    return rhs == lhs;
-}
-
-template <typename T1, typename T2>
-inline bool operator==(const NumericWrapper<T1> &lhs,
-                       const NumericWrapper<T2> &rhs) {
-    if constexpr (std::is_same_v<T1, T2>) {
-        return lhs.value == rhs.value;
-    } else {
-        static_assert(std::is_same_v<T1, double> && std::is_same_v<T2, double>);
-        return lhs.fp64() == rhs.fp64();
-    }
-}
-
-/* Arithmetic operators */
-/* operator+() */
-template <typename T, RoundingMode R>
-static inline NumericWrapper<T, R> operator+(const NumericWrapper<T, R> &lhs,
-                                             const T &rhs) {
+// Relational operators
+template <typename T, rfp::RoundingMode R>
+FEATURE_CXX20(constexpr)
+static inline auto operator==(const T &lhs, const NumericWrapper<T, R> &rhs) {
     static_assert(std::numeric_limits<T>::is_iec559);
-    SAFE_BINOP(result, lhs.value, rhs, +);
-    return NumericWrapper(result);
+    return NumericWrapper(lhs) == rhs;
 }
 
-template <typename T, RoundingMode R>
+template <typename T, rfp::RoundingMode R>
+FEATURE_CXX20(constexpr)
+static inline auto operator!=(const T &lhs, const NumericWrapper<T, R> &rhs) {
+    static_assert(std::numeric_limits<T>::is_iec559);
+    return NumericWrapper(lhs) != rhs;
+}
+
+template <typename T, rfp::RoundingMode R>
+FEATURE_CXX20(constexpr)
+static inline auto operator<=(const T &lhs, const NumericWrapper<T, R> &rhs) {
+    static_assert(std::numeric_limits<T>::is_iec559);
+    return NumericWrapper(lhs) <= rhs;
+}
+
+template <typename T, rfp::RoundingMode R>
+FEATURE_CXX20(constexpr)
+static inline auto operator>=(const T &lhs, const NumericWrapper<T, R> &rhs) {
+    static_assert(std::numeric_limits<T>::is_iec559);
+    return NumericWrapper(lhs) >= rhs;
+}
+
+template <typename T, rfp::RoundingMode R>
+FEATURE_CXX20(constexpr)
+static inline auto operator>(const T &lhs, const NumericWrapper<T, R> &rhs) {
+    static_assert(std::numeric_limits<T>::is_iec559);
+    return NumericWrapper(lhs) > rhs;
+}
+
+template <typename T, rfp::RoundingMode R>
+FEATURE_CXX20(constexpr)
+static inline auto operator<(const T &lhs, const NumericWrapper<T, R> &rhs) {
+    static_assert(std::numeric_limits<T>::is_iec559);
+    return NumericWrapper(lhs) < rhs;
+}
+
+// Arithmetic operators
+template <typename T, rfp::RoundingMode R>
+FEATURE_CXX20(constexpr)
 static inline NumericWrapper<T, R> operator+(const T &lhs,
                                              const NumericWrapper<T, R> &rhs) {
     static_assert(std::numeric_limits<T>::is_iec559);
-    SAFE_BINOP(result, lhs, rhs.value, +);
-    return NumericWrapper(result);
+    return NumericWrapper(lhs) + rhs;
 }
 
-template <typename T, RoundingMode R>
-static inline NumericWrapper<T, R> operator+(const NumericWrapper<T, R> &lhs,
-                                             const NumericWrapper<T, R> &rhs) {
-    SAFE_BINOP(result, lhs.value, rhs.value, +);
-    return NumericWrapper(result);
-}
-
-/* operator-() */
-template <typename T, RoundingMode R>
-static inline NumericWrapper<T, R> operator-(const NumericWrapper<T, R> &lhs,
-                                             const T &rhs) {
-    static_assert(std::numeric_limits<T>::is_iec559);
-    SAFE_BINOP(result, lhs.value, rhs, -);
-    return NumericWrapper(result);
-}
-
-template <typename T, RoundingMode R>
+template <typename T, rfp::RoundingMode R>
+FEATURE_CXX20(constexpr)
 static inline NumericWrapper<T, R> operator-(const T &lhs,
                                              const NumericWrapper<T, R> &rhs) {
     static_assert(std::numeric_limits<T>::is_iec559);
-    SAFE_BINOP(result, lhs, rhs.value, -);
-    return NumericWrapper(result);
+    return NumericWrapper(lhs) - rhs;
 }
 
-template <typename T, RoundingMode R>
-static inline NumericWrapper<T, R> operator-(const NumericWrapper<T, R> &lhs,
-                                             const NumericWrapper<T, R> &rhs) {
-    SAFE_BINOP(result, lhs.value, rhs.value, -);
-    return NumericWrapper(result);
-}
-
-/* operator*() */
-template <typename T, RoundingMode R>
-static inline NumericWrapper<T, R> operator*(const NumericWrapper<T, R> &lhs,
-                                             const T &rhs) {
-    static_assert(std::numeric_limits<T>::is_iec559);
-    SAFE_BINOP(result, lhs.value, rhs, *);
-    return NumericWrapper(result);
-}
-
-template <typename T, RoundingMode R>
+template <typename T, rfp::RoundingMode R>
+FEATURE_CXX20(constexpr)
 static inline NumericWrapper<T, R> operator*(const T &lhs,
                                              const NumericWrapper<T, R> &rhs) {
     static_assert(std::numeric_limits<T>::is_iec559);
-    SAFE_BINOP(result, lhs, rhs.value, *);
-    return NumericWrapper(result);
+    return NumericWrapper(lhs) * rhs;
 }
 
-template <typename T, RoundingMode R>
-static inline NumericWrapper<T, R> operator*(const NumericWrapper<T, R> &lhs,
-                                             const NumericWrapper<T, R> &rhs) {
-    SAFE_BINOP(result, lhs.value, rhs.value, *);
-    return NumericWrapper(result);
-}
-
-/* operator/() */
-template <typename T, RoundingMode R>
-static inline NumericWrapper<T, R> operator/(const NumericWrapper<T, R> &lhs,
-                                             const T &rhs) {
-    static_assert(std::numeric_limits<T>::is_iec559);
-    SAFE_BINOP(result, lhs.value, rhs, /);
-    return NumericWrapper(result);
-}
-
-template <typename T, RoundingMode R>
+template <typename T, rfp::RoundingMode R>
+FEATURE_CXX20(constexpr)
 static inline NumericWrapper<T, R> operator/(const T &lhs,
                                              const NumericWrapper<T, R> &rhs) {
     static_assert(std::numeric_limits<T>::is_iec559);
-    SAFE_BINOP(result, lhs, rhs.value, /);
-    return NumericWrapper(result);
-}
-
-template <typename T, RoundingMode R>
-static inline NumericWrapper<T, R> operator/(const NumericWrapper<T, R> &lhs,
-                                             const NumericWrapper<T, R> &rhs) {
-    SAFE_BINOP(result, lhs.value, rhs.value, /);
-    return NumericWrapper(result);
+    return NumericWrapper(lhs) / rhs;
 }
 
 #undef OPT_BARRIER
--- src/reproducible_float_tests.cpp
+++ src/reproducible_float_tests.cpp
@@ -13,53 +13,100 @@ class InterfaceTest : public ::testing::Test {
 
 // Tests for rdouble
 TEST_F(InterfaceTest, rdouble_arithmetic) {
-    rdouble dw1(d1);
-    rdouble dw2(d2);
+    rdouble rd1(d1);
+    rdouble rd2(d2);
 
-    EXPECT_EQ(dw1 + dw2, d1 + d2);
-    EXPECT_EQ(dw1 - dw2, d1 - d2);
-    EXPECT_EQ(dw1 * dw2, d1 * d2);
-    EXPECT_EQ(dw1 / dw2, d1 / d2);
+    EXPECT_EQ(rd1 + rd2, d1 + d2);
+    EXPECT_EQ(rd1 - rd2, d1 - d2);
+    EXPECT_EQ(rd1 * rd2, d1 * d2);
+    EXPECT_EQ(rd1 / rd2, d1 / d2);
 }
 
-TEST_F(InterfaceTest, rfloat_pod_assignment) {
-    rdouble fw1(f1);
-    rdouble fw2(f1);
-    fw1 = f2;
-    fw2 = fw1;
+TEST_F(InterfaceTest, rfloat_assignment) {
+    rfloat rf1(f1);
+    rfloat rf2(f1);
+    rf1 = f2;
+    rf2 = rf1;
 
-    EXPECT_EQ(f2, fw1);
-    EXPECT_EQ(fw1, fw2);
+    EXPECT_EQ(f2, rf1);
+    EXPECT_EQ(rf1, rf2);
 }
 
 TEST_F(InterfaceTest, rdouble_assignment) {
-    rdouble dw1(d1);
-    rdouble dw2(d1);
-    dw1 = d2;
-    dw2 = dw1;
+    rdouble rd1(d1);
+    rdouble rd2(d1);
+    rd1 = d2;
+    rd2 = rd1;
 
-    EXPECT_EQ(d2, dw1);
-    EXPECT_EQ(dw1, dw2);
+    EXPECT_EQ(d2, rd1);
+    EXPECT_EQ(rd1, rd2);
+}
+
+TEST_F(InterfaceTest, rfloat_relational) {
+    rfloat rf1(f1);
+    rfloat rf2(f2);
+
+    EXPECT_LT(rf2, rf1);
+    EXPECT_GT(rf1, rf2);
+    EXPECT_LE(rf1, rf1);
+    EXPECT_GE(rf2, rf2);
+    EXPECT_NE(rf1, rf2);
+
+    EXPECT_LT(rf2, f1);
+    EXPECT_GT(rf1, f2);
+    EXPECT_LE(rf1, f1);
+    EXPECT_GE(rf2, f2);
+    EXPECT_NE(rf1, f2);
+
+    // Make sure the reverse expression also compiles
+    EXPECT_GT(f1, rf2);
+    EXPECT_LT(f2, rf1);
+    EXPECT_GE(f1, rf1);
+    EXPECT_LE(f2, f2);
+    EXPECT_NE(f1, rf2);
+}
+
+TEST_F(InterfaceTest, rdouble_relational) {
+    rdouble rd1(d1);
+    rdouble rd2(d2);
+
+    EXPECT_LT(rd2, rd1);
+    EXPECT_GT(rd1, rd2);
+    EXPECT_LE(rd1, rd1);
+    EXPECT_GE(rd2, rd2);
+    EXPECT_NE(rd1, rd2);
+
+    EXPECT_LT(rd2, d1);
+    EXPECT_GT(rd1, d2);
+    EXPECT_LE(rd1, d1);
+    EXPECT_GE(rd2, d2);
+    EXPECT_NE(rd1, d2);
+
+    EXPECT_GT(d1, rd2);
+    EXPECT_LT(d2, rd1);
+    EXPECT_GE(d1, rd1);
+    EXPECT_LE(d2, d2);
+    EXPECT_NE(d1, rd2);
 }
 
 TEST_F(InterfaceTest, rdouble_conversion) {
-    rdouble dw(d1);
-    EXPECT_EQ(d1, dw.fp64());
+    rdouble rd1(d1);
+    EXPECT_EQ(d1, rd1.fp64());
 
     // This should generate a compile error,
     // but GTest has no way to verify that
-    // EXPECT_EQ(static_cast<float>(d1), dw.fp32());
+    // EXPECT_EQ(static_cast<float>(d1), rd1.fp32());
 }
 
 // Tests for rfloat
 TEST_F(InterfaceTest, rfloat_arithmetic) {
-    rfloat fw1(f1);
-    rfloat fw2(f2);
+    rfloat rf1(f1);
+    rfloat rf2(f2);
 
-    EXPECT_EQ(fw1 + fw2, f1 + f2);
-    EXPECT_EQ(fw1 - fw2, f1 - f2);
-    EXPECT_EQ(fw1 * fw2, f1 * f2);
-    EXPECT_EQ(fw1 / fw2, f1 / f2);
+    EXPECT_EQ(rf1 + rf2, f1 + f2);
+    EXPECT_EQ(rf1 - rf2, f1 - f2);
+    EXPECT_EQ(rf1 * rf2, f1 * f2);
+    EXPECT_EQ(rf1 / rf2, f1 / f2);
 }
 
 TEST_F(InterfaceTest, rfloat_conversion) {
@@ -69,50 +116,87 @@ TEST_F(InterfaceTest, rfloat_conversion) {
 }
 
 TEST_F(InterfaceTest, rdouble_move_semantics) {
-    rdouble dw1(d1);
-    rdouble dw2(d2);
+    rdouble rd1(d1);
+    rdouble rd2(d2);
 
-    EXPECT_EQ(std::move(dw1) + dw2, d1 + d2);
-    EXPECT_EQ(std::move(dw1) - dw2, d1 - d2);
-    EXPECT_EQ(std::move(dw1) * dw2, d1 * d2);
-    EXPECT_EQ(std::move(dw1) / dw2, d1 / d2);
+    EXPECT_EQ(std::move(rd1) + rd2, d1 + d2);
+    EXPECT_EQ(std::move(rd1) - rd2, d1 - d2);
+    EXPECT_EQ(std::move(rd1) * rd2, d1 * d2);
+    EXPECT_EQ(std::move(rd1) / rd2, d1 / d2);
 
     // Test reversed commutative operations
-    EXPECT_EQ(dw1 + std::move(dw2), d1 + d2);
-    EXPECT_EQ(dw1 * std::move(dw2), d1 * d2);
+    EXPECT_EQ(rd1 + std::move(rd2), d1 + d2);
+    EXPECT_EQ(rd1 * std::move(rd2), d1 * d2);
 
     // The inverted - and / operators should generate compile
     // errors, but GTest has no way to verify that
 }
 
 TEST_F(InterfaceTest, rfloat_move_semantics) {
-    rfloat fw1(f1);
-    rfloat fw2(f2);
+    rfloat rf1(f1);
+    rfloat rf2(f2);
 
-    EXPECT_EQ(std::move(fw1) - fw2, f1 - f2);
-    EXPECT_EQ(std::move(fw1) * fw2, f1 * f2);
-    EXPECT_EQ(std::move(fw1) + fw2, f1 + f2);
-    EXPECT_EQ(std::move(fw1) / fw2, f1 / f2);
+    EXPECT_EQ(std::move(rf1) - rf2, f1 - f2);
+    EXPECT_EQ(std::move(rf1) * rf2, f1 * f2);
+    EXPECT_EQ(std::move(rf1) + rf2, f1 + f2);
+    EXPECT_EQ(std::move(rf1) / rf2, f1 / f2);
 
     // Test reversed commutative operations
-    EXPECT_EQ(fw1 + std::move(fw2), f1 + f2);
-    EXPECT_EQ(fw1 * std::move(fw2), f1 * f2);
+    EXPECT_EQ(rf1 + std::move(rf2), f1 + f2);
+    EXPECT_EQ(rf1 * std::move(rf2), f1 * f2);
 
     // The inverted - and / operators should generate compile
     // errors, but GTest has no way to verify that
 }
 
 TEST_F(InterfaceTest, test_lhs_rhs_insensitivity) {
-    rfloat fw1(f1);
-    rdouble dw1(d1);
+    rfloat rf1(f1);
+    rdouble rd1(d1);
 
-    EXPECT_EQ(d1 + dw1, dw1 + d1);
-    EXPECT_EQ(f1 + fw1, fw1 + f1);
+    EXPECT_EQ(d1 + rd1, rd1 + d1);
+    EXPECT_EQ(f1 + rf1, rf1 + f1);
+}
+
+TEST_F(InterfaceTest, test_arithmetic_different_types) {
+    rfloat rf1(f1);
+    rdouble rd1(d1);
+
+    // There's an implicit narrowing conversion that happens here
+    // because we've defined convenience functions. Unfortunately,
+    // there's no way to prevent that narrowing conversion in
+    // the library, so watch out
+    EXPECT_EQ(rf1 + d1, f1 + d1);
+    EXPECT_EQ(rd1 + f1, f1 + d1);
 }
 
 // This should generate compile errors
-// TEST_F(InterfaceTest, validate_non_iec559) {
-//     NumericWrapper<long double> dw1(f1);
+// TEST_F(InterfaceTest, check_mixed_types_prohibited) {
+//     rfloat rf1(f1);
+//     rdouble rd1(d1);
 
-//     EXPECT_EQ(dw1 + d1, dw1 + d1);
+//     EXPECT_EQ(rd1 + rf1, d1 + f1);
+// }
+
+// This should generate compile errors
+// TEST_F(InterfaceTest, test_equality_static_failure) {
+//     rfloat rf1(f1);
+//     int i = 0;
+
+//     EXPECT_TRUE(rf1 == i);
+// }
+
+// This should generate compile errors
+// TEST_F(InterfaceTest, validate_non_iec559) {
+//     NumericWrapper<long double> rd1(f1);
+
+//     EXPECT_EQ(rd1 + d1, rd1 + d1);
+// }
+
+// This should generate compile errors because the cast functions are deleted
+// TEST_F(InterfaceTest, c_cast_fails) {
+//     rfloat rf1(f1);
+//     rdouble rd1(d1);
+
+//     EXPECT_EQ((float)rf1, f1);
+//     EXPECT_EQ((double)rd1, d1);
 // }
