41d6766969d5c8694c8f1e240b2ba10a1935f962
J M <montytyper@msn.com>
2024-10-05 23:32:51 -0700

    Move some of the infrastructure out of reproducibility_tests code

--- src/reproducibility_tests.cpp
+++ src/reproducibility_tests.cpp
@@ -1,12 +1,8 @@
-#include <array>
-#include <gtest/gtest.h>
-#include <sstream>
-#include <stdexcept>
-#include <vector>
-
 #include "rcmath_tests.hh"
 #include <rfloat/rcmath.hh>
 #include <rfloat/rfloat.hh>
+
+#include "reproducibility_tests.hh"
 
 #include "testdata/random_logistic_map_testdata.hh"
 #include "testdata/random_lorenz_testdata.hh"
@@ -17,69 +13,12 @@
 #include "testdata/random_floor_testdata.hh"
 #include "testdata/random_fma_testdata.hh"
 
-// Infrastructure to read lines of test data from the headers
-// included above and parsed into TestParam objects. Each line is a single test
-// case.
-template <typename T, std::size_t InputSize, std::size_t OutputSize>
-struct TestParam {
-    std::array<T, InputSize> inputs;
-    std::array<T, OutputSize> expected_outputs;
-};
-
-template <typename T, std::size_t InputSize, std::size_t OutputSize>
-class HeaderParameterizedTest : public ::testing::Test {
-  public:
-    static std::vector<TestParam<T, InputSize, OutputSize>>
-    LoadTestData(std::string &data) {
-        std::vector<TestParam<T, InputSize, OutputSize>> test_data;
-        std::istringstream iss(data);
-        std::string line;
-        int line_number = 0;
-
-        while (std::getline(iss, line)) {
-            line_number++;
-
-            if (line.find_first_not_of(" \t\r\n") == std::string::npos) {
-                continue;
-            }
-
-            std::istringstream line_stream(line);
-            TestParam<T, InputSize, OutputSize> param;
-
-            for (std::size_t i = 0; i < InputSize; ++i) {
-                if (!(line_stream >> param.inputs[i])) {
-                    throw std::runtime_error(
-                        "Not enough valid input values on line " +
-                        std::to_string(line_number));
-                }
-            }
-
-            for (std::size_t i = 0; i < OutputSize; ++i) {
-                if (!(line_stream >> param.expected_outputs[i])) {
-                    throw std::runtime_error(
-                        "Not enough valid output values on line " +
-                        std::to_string(line_number));
-                }
-            }
-
-            int extra;
-            if (line_stream >> extra) {
-                throw std::runtime_error("Too many values on line " +
-                                         std::to_string(line_number));
-            }
-
-            test_data.push_back(param);
-        }
-        return test_data;
-    }
-};
-
 /* Chaotic function tests*/
 const std::size_t steps = 1000;
 
 TEST(LorenzTest, RandomInputs) {
-    auto test_data = HeaderParameterizedTest<rdouble, 3, 3>::LoadTestData(
-        random_lorenz_testdata);
+    auto test_data =
+        ParameterizedTest<rdouble, 3, 3>::LoadTestData(random_lorenz_testdata);
     using tf = TestFunctions<rdouble>;
     for (const auto &param : test_data) {
         auto results = TestFunctions<rdouble>::lorenz(param.inputs, steps);
@@ -88,7 +27,7 @@ TEST(LorenzTest, RandomInputs) {
 }
 
 TEST(MandelbrotTest, RandomInputs) {
-    auto test_data = HeaderParameterizedTest<rdouble, 2, 2>::LoadTestData(
+    auto test_data = ParameterizedTest<rdouble, 2, 2>::LoadTestData(
         random_mandelbrot_testdata);
     using tf = TestFunctions<rdouble>;
     for (const auto &param : test_data) {
@@ -98,7 +37,7 @@ TEST(MandelbrotTest, RandomInputs) {
 }
 
 TEST(LogisticMapTest, RandomInputs) {
-    auto test_data = HeaderParameterizedTest<rdouble, 2, 1>::LoadTestData(
+    auto test_data = ParameterizedTest<rdouble, 2, 1>::LoadTestData(
         random_logistic_map_testdata);
     using tf = TestFunctions<rdouble>;
     for (const auto &param : test_data) {
@@ -113,8 +52,8 @@ TEST(LogisticMapTest, RandomInputs) {
 
 /* rcmath tests */
 TEST(AbsTest, RandomInputs) {
-    auto test_data = HeaderParameterizedTest<rdouble, 1, 1>::LoadTestData(
-        random_abs_testdata);
+    auto test_data =
+        ParameterizedTest<rdouble, 1, 1>::LoadTestData(random_abs_testdata);
     for (const auto &param : test_data) {
         auto results = rmath::abs(param.inputs[0]);
         EXPECT_EQ(results, param.expected_outputs[0]);
@@ -123,8 +62,8 @@ TEST(AbsTest, RandomInputs) {
 }
 
 TEST(FloorTest, RandomInputs) {
-    auto test_data = HeaderParameterizedTest<rdouble, 1, 1>::LoadTestData(
-        random_floor_testdata);
+    auto test_data =
+        ParameterizedTest<rdouble, 1, 1>::LoadTestData(random_floor_testdata);
     for (const auto &param : test_data) {
         auto results = rmath::floor(param.inputs[0]);
         EXPECT_EQ(results, param.expected_outputs[0]);
@@ -133,8 +72,8 @@ TEST(FloorTest, RandomInputs) {
 }
 
 TEST(CeilTest, RandomInputs) {
-    auto test_data = HeaderParameterizedTest<rdouble, 1, 1>::LoadTestData(
-        random_ceil_testdata);
+    auto test_data =
+        ParameterizedTest<rdouble, 1, 1>::LoadTestData(random_ceil_testdata);
     for (const auto &param : test_data) {
         auto results = rmath::ceil(param.inputs[0]);
         EXPECT_EQ(results, param.expected_outputs[0]);
@@ -143,8 +82,8 @@ TEST(CeilTest, RandomInputs) {
 }
 
 TEST(FmaTest, RandomInputs) {
-    auto test_data = HeaderParameterizedTest<rdouble, 3, 1>::LoadTestData(
-        random_fma_testdata);
+    auto test_data =
+        ParameterizedTest<rdouble, 3, 1>::LoadTestData(random_fma_testdata);
     for (const auto &param : test_data) {
         auto results =
             rmath::fma(param.inputs[0], param.inputs[1], param.inputs[2]);
---
+++ src/reproducibility_tests.hh
@@ -0,0 +1,65 @@
+#pragma once
+
+#include <array>
+#include <gtest/gtest.h>
+#include <sstream>
+#include <stdexcept>
+#include <vector>
+
+
+// Infrastructure to read lines of test data from the headers
+// included above and parsed into TestParam objects. Each line is a single test
+// case.
+template <typename T, std::size_t InputSize, std::size_t OutputSize>
+struct TestParam {
+    std::array<T, InputSize> inputs;
+    std::array<T, OutputSize> expected_outputs;
+};
+
+template <typename T, std::size_t InputSize, std::size_t OutputSize>
+class ParameterizedTest : public ::testing::Test {
+  public:
+    static std::vector<TestParam<T, InputSize, OutputSize>>
+    LoadTestData(std::string &data) {
+        std::vector<TestParam<T, InputSize, OutputSize>> test_data;
+        std::istringstream iss(data);
+        std::string line;
+        int line_number = 0;
+
+        while (std::getline(iss, line)) {
+            line_number++;
+
+            if (line.find_first_not_of(" \t\r\n") == std::string::npos) {
+                continue;
+            }
+
+            std::istringstream line_stream(line);
+            TestParam<T, InputSize, OutputSize> param;
+
+            for (std::size_t i = 0; i < InputSize; ++i) {
+                if (!(line_stream >> param.inputs[i])) {
+                    throw std::runtime_error(
+                        "Not enough valid input values on line " +
+                        std::to_string(line_number));
+                }
+            }
+
+            for (std::size_t i = 0; i < OutputSize; ++i) {
+                if (!(line_stream >> param.expected_outputs[i])) {
+                    throw std::runtime_error(
+                        "Not enough valid output values on line " +
+                        std::to_string(line_number));
+                }
+            }
+
+            int extra;
+            if (line_stream >> extra) {
+                throw std::runtime_error("Too many values on line " +
+                                         std::to_string(line_number));
+            }
+
+            test_data.push_back(param);
+        }
+        return test_data;
+    }
+};
